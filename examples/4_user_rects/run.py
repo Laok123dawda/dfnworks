4bb3b980b25f4f142b879c96f83ff3445a56dc79
Jeffrey Hyman <jhyman@lanl.gov>
2022-07-06 18:55:56 -0600

    including set_fracture_hydraulic_values

--- examples/4_user_rects/run.py
+++ examples/4_user_rects/run.py
@@ -11,5 +11,8 @@ from pydfnworks import *
 DFN = create_dfn()
 # General Work Flow
 DFN.dfn_gen()
+fracture_list = list(range(1,5))
+apertures = 4*[1e-4]
+DFN.set_fracture_hydraulic_values("aperture", fracture_list, apertures)
 DFN.dfn_flow()
 DFN.dfn_trans()
